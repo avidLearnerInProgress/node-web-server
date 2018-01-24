8e6d904612ca76118a1eff402d295c9408789c8e
avidLearnerInProgress <chiragshah9696@gmail.com>
2018-01-24 11:14:16 +0530

    Added Files

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+//sets handler for HTTP get request
+//get(url, function to run)
+
+
+//partials --> updates a specific part of code in all the pages
+//footer is loaded in both about.hbs and home.hbs via partials
+hbs.registerPartials(__dirname + '/views/partials');
+
+//handlebars
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+
+	var now = new Date().toString();
+	var log = (`${now}: ${req.method} ${req.url}`);
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => { 
+		if(err) console.log(err);
+	});
+	next();
+});
+
+app.use((req,res, next) => {
+	res.render('maintainence.hbs');
+});
+
+//middleware //app.use
+app.use(express.static(__dirname + '/public'));
+
+
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+
+});
+
+
+app.get('/', (req, res) =>{
+	//res.send('<h1>Hello Express</h1>');
+
+	res.render('home.hbs',{
+		pageTitle : 'Home Page',
+		
+		welcomeMessage : 'Welcome to home page'
+
+	});
+});
+
+app.get('/about', (req, res) =>{
+	res.render('about.hbs',{
+		pageTitle : 'About Page',
+		
+	});
+});
+
+
+app.get('/bad', (req, res) =>{
+	res.send({
+		error: 'Error!'
+	});
+});
+
+app.listen(3000, () =>{
+	console.log('Server running on port 3000');
+});
